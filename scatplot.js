2f7729221841bb8df50079148b92de5a6af6af7e
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2020-02-03 22:30:21 +0800

    config file re-structure; support extra static traces

--- scatplot.js
+++ scatplot.js
@@ -4,15 +4,22 @@
 // eval using mathjs : https://mathjs.org/examples/basic_usage.js.html
 
 var G = {
-  'config': {
+  'source': {
     'keep': [ 'true' ],
     'extracols': [],
   },
   'table': {
-    'invd': {}, // internal numerical variable dictionary
-	        // for mapping utf8 strings to alphanumeric names
-	        // for math evaluation
-  }
+    'invd': {},
+  },
+  'plotly': {
+    'statictraces': [],
+    'config': {
+      toImageButtonOptions: {
+        format: 'svg',
+        scale: 1.2
+      }
+    },
+  },
 };
 
 const pKeyPrefix='k';
@@ -23,9 +30,24 @@ function rndsfx() {
   return '?' + Math.floor(Math.random() * 1000);
 }
 
-G.url = new URI(location.href);
-G.urlConfig = G.url.search(true);
-if (! G.urlConfig.c) { G.urlConfig.c='config.json'; }
+function cn2val(text, dict) {
+  // substitute column names in a text string with dict values
+  for (var cn in dict) {
+    var re = new RegExp(cn, 'g');
+    text = text.replace(re, dict[cn]);
+  }
+  return text;
+}
+
+function u8varMathEval(expr, dict, u2adict) {
+  var k, anDict = {};	// alphanumeric expr and alpha numeric dict
+  for (k in u2adict) {
+    var re = new RegExp(k, 'g');
+    expr = expr.replace(re, u2adict[k]);
+    anDict[u2adict[k]] = dict[k];
+  }
+  return math.eval(expr, anDict);
+}
 
 // 出處： https://stackoverflow.com/questions/41440945/handling-dynamic-arguments-from-when-in-jquery 超重要！
 $.whenAll = function(promises) {
@@ -40,19 +62,6 @@ $.whenAll = function(promises) {
   });
   return d.promise();
 };
-
-$.getJSON(G.urlConfig.c, function(cfgdata) {
-  // set up config
-  $.extend(G.config, cfgdata);
-  $.extend(G.config, G.urlConfig);
-  if (! Array.isArray(G.config.keep)) { G.config.keep = [ G.config.keep ]; }
-  if (! Array.isArray(G.config.csv)) { G.config.csv = [ G.config.csv ]; }
-  var csvReq = G.config.csv.map(function (url) { return $.get(url+rndsfx()); });
-  $.whenAll(csvReq).then(initB);
-  // 最有用： https://stackoverflow.com/questions/41440945/handling-dynamic-arguments-from-when-in-jquery 超級讚！
-  // https://stackoverflow.com/questions/14352139/multiple-ajax-calls-from-array-and-handle-callback-when-completed
-  // https://stackoverflow.com/questions/4878887/how-do-you-work-with-an-array-of-jquery-deferreds
-});
 
 function parseCSV(str, textcols) {
   // it's amazing how difficult it is to find
@@ -75,31 +84,12 @@ function parseCSV(str, textcols) {
   return ret;
 }
 
-function u8varMathEval(expr, dict, u2adict) {
-  var k, anDict = {};	// alphanumeric expr and alpha numeric dict
-  for (k in u2adict) {
-    var re = new RegExp(k, 'g');
-    expr = expr.replace(re, u2adict[k]);
-    anDict[u2adict[k]] = dict[k];
-  }
-  return math.eval(expr, anDict);
-}
-
-function cn2val(text, dict) {
-  // substitute column names in a text string with dict values
-  for (var cn in dict) {
-    var re = new RegExp(cn, 'g');
-    text = text.replace(re, dict[cn]);
-  }
-  return text;
-}
-
 function redraw() {
 
   var // aliases for global variables
     tableContent,
     filteredData,
-    plotlyLayout = G.config.plotly;
+    pltMainTrace = G.plotly.maintrace;
 
   $.fn.dataTable.ext.search = [
     function(settings, row, index) {
@@ -127,50 +117,49 @@ function redraw() {
     return newrow;
   });
 
-  plotlyLayout.xaxis.expr = $('#X_expr').val();
-  plotlyLayout.xaxis.title = { 'text': plotlyLayout.xaxis.expr };
-  plotlyLayout.yaxis.expr = $('#Y_expr').val();
-  plotlyLayout.yaxis.title = { 'text': plotlyLayout.yaxis.expr };
-  // plotlyLayout.zaxis.expr = $('#Z_expr').val();
-  // plotlyLayout.zaxis.title = { 'text': plotlyLayout.zaxis.expr };
-  plotlyLayout.size.expr = $('#Size_expr').val();
+  pltMainTrace.xaxis.expr = $('#X_expr').val();
+  pltMainTrace.xaxis.title = { 'text': pltMainTrace.xaxis.expr };
+  pltMainTrace.yaxis.expr = $('#Y_expr').val();
+  pltMainTrace.yaxis.title = { 'text': pltMainTrace.yaxis.expr };
+  // pltMainTrace.zaxis.expr = $('#Z_expr').val();
+  // pltMainTrace.zaxis.title = { 'text': pltMainTrace.zaxis.expr };
+  pltMainTrace.size.expr = $('#Size_expr').val();
 
-  // first generate a plotlyLayout trace
   var maintext = tableContent.map(function (row) {
-    return cn2val(plotlyLayout.maintext, row);
+    return cn2val(pltMainTrace.maintext, row);
   });
   var hovertext = tableContent.map(function (row) {
-    return cn2val(plotlyLayout.hovertext, row);
+    return cn2val(pltMainTrace.hovertext, row);
   });
-  var trace = {
+  var mainTrace = {
     // https://plot.ly/javascript/bubble-charts/
     // https://plot.ly/javascript/reference/
-    'type': 'scatter3d',
-    'x': tableContent.map(function (row) { return u8varMathEval(plotlyLayout.xaxis.expr, row, G.table.invd); } ),
-    'y': tableContent.map(function (row) { return u8varMathEval(plotlyLayout.yaxis.expr, row, G.table.invd); } ),
-    'name': plotlyLayout.xaxis.expr + ' / ' + plotlyLayout.yaxis.expr,
+    'type': 'scatter',
+    'x': tableContent.map(function (row) { return u8varMathEval(pltMainTrace.xaxis.expr, row, G.table.invd); } ),
+    'y': tableContent.map(function (row) { return u8varMathEval(pltMainTrace.yaxis.expr, row, G.table.invd); } ),
+    'name': pltMainTrace.xaxis.expr + ' / ' + pltMainTrace.yaxis.expr,
     'text': maintext,
     'hoverinfo': 'x+y+text',
     'hovertext': hovertext,
-    'mode': plotlyLayout.maintext ? 'markers+text' : 'markers',
+    'mode': pltMainTrace.maintext ? 'markers+text' : 'markers',
     'marker': {
       'symbol': 'circle',
-      'size': tableContent.map(function (row) { return ('size' in plotlyLayout && 'expr' in plotlyLayout.size ? u8varMathEval(plotlyLayout.size.expr, row, G.table.invd) : 10); } ),
+      'size': tableContent.map(function (row) { return ('size' in pltMainTrace && 'expr' in pltMainTrace.size ? u8varMathEval(pltMainTrace.size.expr, row, G.table.invd) : 10); } ),
       'color': 'rgba(255,255,255,0.3)',
       'line': {
 	// https://plot.ly/~alex/455/four-ways-to-change-opacity-of-scatter-markers.embed
-        'color': 'color' in plotlyLayout && 'palette' in plotlyLayout.color ?
+        'color': 'color' in pltMainTrace && 'palette' in pltMainTrace.color ?
 	  tableContent.map(function (row) {
-	    var pal = plotlyLayout.color.palette;
+	    var pal = pltMainTrace.color.palette;
 	    if (typeof(pal) == 'number') {
 	      return '#00f';
 	    } else if (typeof(pal) == 'object') {
-	      var k = plotlyLayout.color.column;
-	      return row[k] in pal ? pal[row[k]] : plotlyLayout.color.default;
+	      var k = pltMainTrace.color.column;
+	      return row[k] in pal ? pal[row[k]] : pltMainTrace.color.default;
 	    } else {
-	      return plotlyLayout.color.default;
+	      return pltMainTrace.color.default;
 	    }
-          }) : plotlyLayout.color.default
+          }) : pltMainTrace.color.default
       }
     },
     // https://plot.ly/python/hover-text-and-formatting/
@@ -181,51 +170,48 @@ function redraw() {
   };
 
   // https://plot.ly/javascript/configuration-options/
-  var cfg = {
-    toImageButtonOptions: {
-      format: 'svg',
-      scale: 1.2
-    }
-};
 
-  Plotly.react($('#main_canvas')[0], [trace], plotlyLayout, cfg);
+  var traces = G.plotly.statictraces.slice();
+  // shallow copy is good enough since this part is never changed
+  traces.unshift(mainTrace);
+  Plotly.react($('#main_canvas')[0], traces, G.plotly.layout, G.plotly.config);
 }
 
 function parseJoin(mainTable, csvText) {
-  var t2 = parseCSV(csvText, G.config.textcols);
+  var t2 = parseCSV(csvText, G.source.textcols);
   // convert t2 array into a dict
   var row, dict2 = {}; 
   for (row of t2) {
-    dict2[row[G.config.pkey]] = row;
+    dict2[row[G.source.pkey]] = row;
   }
-  var exRow = G.config.examplekey ? dict2[G.config.examplekey] : t2[0];
+  var exRow = G.source.samplekey ? dict2[G.source.samplekey] : t2[0];
   for (row of mainTable) {
     var cns = Object.keys(exRow);
     for (var cn of cns) {
       if (! (cn in row)) {
-	row[cn] = dict2[row[G.config.pkey]][cn];
+	row[cn] = dict2[row[G.source.pkey]][cn];
       }
     }
   }
 }
 
-function initB(lotab) {
+function init(lotab) {
   // lotab: list of csv tables
   var i, pk, cn, Ntab = lotab.length;
-  G.table.content = parseCSV(lotab[0], G.config.textcols);
-  var exampleRow;
-  if (G.config.pkey && G.config.examplekey) {
+  G.table.content = parseCSV(lotab[0], G.source.textcols);
+  var sampleRow;
+  if (G.source.pkey && G.source.samplekey) {
     for (i=0; i<G.table.content.length; ++i) {
-      if (G.table.content[i][G.config.pkey] == G.config.examplekey) {
-	exampleRow = G.table.content[i];
+      if (G.table.content[i][G.source.pkey] == G.source.samplekey) {
+	sampleRow = G.table.content[i];
       }
     }
   } else {
-    exampleRow = G.table.content[0];
+    sampleRow = G.table.content[0];
   }
-  var visibleCols = Object.keys(exampleRow);
+  var visibleCols = Object.keys(sampleRow);
   if (Ntab > 1) {
-    pk = G.config.pkey;
+    pk = G.source.pkey;
     if (pk == undefined) {
       alert('You have more than 1 csv files.\nPlease define "pkey" in .json.');
       return;
@@ -234,23 +220,26 @@ function initB(lotab) {
       parseJoin(G.table.content, lotab[i]);
     }
   }
-  G.table.colnames = Object.keys(exampleRow);
+  G.table.colnames = Object.keys(sampleRow);
   var colDefs = G.table.colnames.map(function (cn) {
       return {
 	'title': cn,
-	'type': G.config.textcols.includes(cn) ? 'string' : 'num',
+	'type': G.source.textcols.includes(cn) ? 'string' : 'num',
 	'visible': visibleCols.includes(cn),
       };
   } );
+
+  // G.table.invd: internal numerical variable dictionary
+  // for mapping utf8 strings to alphanumeric names for math evaluation
   var invdIndex = 1000;
   G.table.colnames.forEach(function (cn) {
-    if (! G.config.textcols.includes(cn)) {
+    if (! G.source.textcols.includes(cn)) {
       G.table.invd[cn] = 'inv4me' + invdIndex.toString().substr(1);
       ++invdIndex;
     }
   });
 
-  for (var coldef of G.config.extracols) {
+  for (var coldef of G.source.extracols) {
     if (! coldef.includes('=')) { coldef += '=' + coldef; }
     var m = coldef.match(/(.+?)=(.*)/);
     var colname=m[1], expr=m[2];
@@ -267,7 +256,7 @@ function initB(lotab) {
     }
   }
   colDefs.forEach(function (col) {
-    if (! G.config.textcols.includes(col.title)) {
+    if (! G.source.textcols.includes(col.title)) {
       col.orderSequence = [ "desc", "asc" ];
     }
   });
@@ -296,7 +285,7 @@ console.log(G);
   $('#Y_expr_options').html(allColnameOptions);
   // $('#Z_expr_options').html(allColnameOptions);
   $('#Size_expr_options').html(allColnameOptions);
-  $('#keep_expr_options').html(G.config.keep.map(function (kc) {
+  $('#keep_expr_options').html(G.source.keep.map(function (kc) {
     return '<option>' + kc + '\n';
   }));
 
@@ -308,13 +297,30 @@ console.log(G);
 
   $('#redraw').click(redraw);
 
-  $('#keep_expr').val( G.config.keep[0] );
-  $('#X_expr').val( G.config.plotly.xaxis.expr );
-  $('#Y_expr').val( G.config.plotly.yaxis.expr );
-  // $('#Z_expr').val( G.config.plotly.zaxis.expr );
-  $('#Size_expr').val( G.config.plotly.size.expr );
+  $('#keep_expr').val( G.source.keep[0] );
+  $('#X_expr').val( G.plotly.maintrace.xaxis.expr );
+  $('#Y_expr').val( G.plotly.maintrace.yaxis.expr );
+  // $('#Z_expr').val( G.plotly.maintrace.zaxis.expr );
+  $('#Size_expr').val( G.plotly.maintrace.size.expr );
   redraw();
-
 }
 
+////////////////////////////////////////////////////////////
+
+G.url = new URI(location.href);
+G.urlConfig = G.url.search(true);
+if (! G.urlConfig.c) { G.urlConfig.c='config.json'; }
+
+$.getJSON(G.urlConfig.c, function(cfgdata) {
+  // set up config
+  $.extend(true, G, cfgdata);
+  $.extend(true, G, G.urlConfig);
+  if (! Array.isArray(G.source.csv)) { G.source.csv = [ G.source.csv ]; }
+  if (! Array.isArray(G.source.keep)) { G.source.keep = [ G.source.keep ]; }
+  var csvReq = G.source.csv.map(function (url) { return $.get(url+rndsfx()); });
+  $.whenAll(csvReq).then(init);
+  // 最有用： https://stackoverflow.com/questions/41440945/handling-dynamic-arguments-from-when-in-jquery 超級讚！
+  // https://stackoverflow.com/questions/14352139/multiple-ajax-calls-from-array-and-handle-callback-when-completed
+  // https://stackoverflow.com/questions/4878887/how-do-you-work-with-an-array-of-jquery-deferreds
+});
 
